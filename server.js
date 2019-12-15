1e69745c8f0cce292130ba9aad1565690420d566
Glitch (jolly-cheek) <none>
2019-12-15 19:07:44 +0000

    🏣🌁 Checkpoint ./server.js:9182067/105

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const cors = require('cors')
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.use(cors())
 
