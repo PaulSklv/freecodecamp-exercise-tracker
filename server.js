f0d19bf4208090a885d552f65307d3d6d62d8a0f
Glitch (jolly-cheek) <none>
2019-12-15 18:55:51 +0000

    👂🏍 Checkpoint ./package.json:9182067/7019 ./server.js:9182067/108

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.Promise = global.Promise;
+
 mongoose.connect(process.env.MONGO_URI)
 
 app.use(cors())
